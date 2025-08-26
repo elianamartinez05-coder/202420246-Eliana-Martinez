81d9064e4b5877983beb4f316bfde4a1d8448524
elianamartinez05-coder <eliana.martinez05@uptc.edu.co>
2025-08-26 15:47:05 +0000

    Nuevo Hello World con cin y cout

---
+++ helloWorld/HelloWord3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string userName;
+    int userAge;
+    cout << "ingrese su nombre" <<endl;
+    cin >> userName;
+    cout <<"ingrese su edad" <<endl;
+    cin >> userAge;
+    cout << "Hola " << userName << endl;
+    cout << "Su edad es: " << userAge << endl;
+    return 0;    
+}
