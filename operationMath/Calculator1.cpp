cef116b6f8fa6dafcbcbf0532811e9b464a98834
elianamartinez05-coder <eliana.martinez05@uptc.edu.co>
2025-08-26 20:35:23 +0000

    Cambios numevos

--- operationMath/Calculator1.cpp
+++ operationMath/Calculator1.cpp
@@ -3,18 +3,20 @@
 using namespace std;
 
 int main () {
-    int multiResult, diviResult, modulResult, selectOperation;
+    int multiResult, diviResult, modulResult, sumResult, restResult, selectOperation;
     int Valuer1, Valuer2;
-    int divi, multi, modul;
+    int divi, multi, modul, sum, rst;
     divi = 2;
     multi = 1;
     modul =3;
+    sum = 5;
+    rst = 4;
      
     cout << "Ingrese su primer numero: ";
     cin >> Valuer1;
     cout << "Ingrese su segundo numero: ";
     cin >> Valuer2;
-    cout << "Multiplicacion = 1 y Division = 2" <<endl;
+    cout << "Multiplicacion = 1 y Division = 2 modulo = 3 Suma = 5 Resta = 4" <<endl;
     cout << "Seleccione el tipo de operacion que desea hacer: ";
     cin >> selectOperation;
 
@@ -34,5 +36,16 @@ int main () {
         cout << "El resultado del modulo es: " << modulResult << endl;
 
     } 
-    
+    else if (selectOperation==4) 
+    {
+        restResult = Valuer1-Valuer2;
+        cout << "El resultado de la resta es: " << restResult << endl;
+
+    } 
+    else if (selectOperation==5) 
+    {
+        sumResult = Valuer1+Valuer2;
+        cout << "El resultado de la suma es: " << sumResult << endl;
+
+    } 
 }
