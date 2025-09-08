39ed79b24a286a8b318196d6272dee4808e01a31
elianamartinez05-coder <eliana.martinez05@uptc.edu.co>
2025-09-08 16:31:31 +0000

    calculadora intento 2

--- .vscode/Funcion/Calculator2.0.cpp
+++ .vscode/Funcion/Calculator2.0.cpp
@@ -8,8 +8,52 @@ void messengerNumberOfOperations (int selectUser) {
     cout <<"Multiplicacion = 3" <<endl;
     cout <<"División = 4" <<endl;
     cout <<"Modulo = 5" <<endl;
-     cout <<"selecciona la operacion que deseas realizar: " <<endl;
+    cout <<"selecciona la operacion que deseas realizar: " <<endl;
 } 
+
+void operationCase (int selectUser, int valuer1, int valuer2) {
+
+    int addResult, int subResult, int modulResult, int mulResult, int divResult;
+
+    switch (selectUser)
+    {
+        //suma
+    case 1:
+        addResult = valuer1 + valuer2;
+        cout << valuer1 <<"+" <<valuer2 <<"=" <<addResult <<endl;
+        break;
+
+        //Resta
+    case 2:
+        subResult = valuer1 - valuer2;
+        cout << valuer1 <<"-" << valuer2 <<"=" <<subResult <<endl;
+        break;
+
+        //Multiplicación
+
+    case 3:
+        mulResult = valuer1 * valuer2;
+        cout << valuer1 <<"x" <<valuer2 <<"=" << mulResult <<endl;
+        break;
+
+        //División
+    case 4:
+        divResult= valuer1 / valuer2;
+        cout << valuer1 <<"/" << valuer2 <<"=" <<divResult <<endl;
+        break;
+
+        //Modulo
+    case 5:
+        modulResult = valuer1 % valuer2;
+        cout << valuer1 <<"%" << valuer2 <<"=" <<modulResult <<endl;
+        break;
+    default:
+        
+        cout <<"La opcion seleccionada no existe :/" <<endl;
+
+        break;
+    }
+}
 
 int main() {
     int valuer1, valuer2, selectUser;
@@ -20,7 +64,11 @@ int main() {
     cin >> valuer2;
     cout <<"----------------------------------------------" <<endl;
 
-    messengerNumberOfOperations (selectUser);
+    messengerNumberOfOperations ();
     cin >> selectUser;
+
+    operationCase (selectUser, valuer1, valuer2);
+
+    return 0;
 
 }
