46bcbe2fea49015ed8e684db875910ea50bd81ac
elianamartinez05-coder <eliana.martinez05@uptc.edu.co>
2025-09-12 16:24:22 +0000

    esPar

--- Viernes/esPar.cpp
+++ Viernes/esPar.cpp
@@ -1,2 +1,31 @@
 #include <iostream>
 
+using namespace std;
+
+bool esPar(int number){
+    if (number % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main(){
+    int userNumber;
+    bool isUserNumber;
+    cout <<"ingrese un numero" <<endl;
+    cin >> userNumber;
+    isUserNumber = esPar(userNumber);
+    if(isUserNumber)
+    {
+        cout <<"El numero es Par" <<endl;
+    }
+    else
+    {
+        cout <<"El numero es Impar" <<endl;
+    }
+    return 0;
+}
