7df6cbc18b19adf536f1de34963a113eecf35777
elianamartinez05-coder <eliana.martinez05@uptc.edu.co>
2025-09-16 19:21:50 +0000

    ejercicio 2

---
+++ PARCIAL-PRIMER50-MICHELLE-MARTINEZ-202420246/ejercicio2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void shiftsWorked(int turn1, int turn2){
+    if (turn1<=turn2)
+    {
+        int counter = turn1;
+        cout << "La maquina trajajó los turnos: " ;
+        while (counter <= turn2)
+        {
+            cout <<counter <<" ";
+            counter ++;
+        }
+        
+    }
+    else
+    {
+        cout <<"Error: el turno inicial no puede ser mayor que el final." <<endl;
+    }
+}
+
+int main(){
+    int initialTurn, finalTurn;
+    cout << "¿En que turno inició?: "<<endl;
+    cin>> initialTurn;
+    cout <<"¿En que turno acabó?" <<endl;
+    cin >> finalTurn;
+
+    shiftsWorked(initialTurn, finalTurn);
+
+    return 0;
+}
