2687e06aa02f6242f650a92e15eca0ead541c5fb
elianamartinez05-coder <eliana.martinez05@uptc.edu.co>
2025-09-16 15:41:30 +0000

    contadorWhile

---
+++ TallerRepaso/contadorWhile.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void counter(int number){
+    int contador=0;
+    while(contador <= number)
+    {
+        cout << contador <<endl;
+        contador ++;
+        
+    }
+}
+
+void counterRegressive(int number){
+    int contador = number;
+    while (contador >= 0)
+    {
+        cout << contador <<endl;
+        contador --;
+    }
+    
+}
+
+int main(){
+    int userNumber;
+    cout <<"escriba el limite de contador" <<endl;
+    cin >> userNumber;
+    cout <<"_________________________________________" <<endl;
+
+    counter(userNumber);
+
+    cout <<"_________________________________________" <<endl;
+
+    counterRegressive(userNumber);
+    
+    return 0;
+}
