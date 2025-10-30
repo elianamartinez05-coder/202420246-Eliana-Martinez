aedd4a4b9d3c466f56d626ed19e30b074b33ec1d
elianamartinez05-coder <eliana.martinez05@uptc.edu.co>
2025-10-30 16:54:34 +0000

    ejercicio jsjs

--- Taller#1-Array/ejercicio4.cpp
+++ Taller#1-Array/ejercicio4.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 void evenNumber(){
     int number;
+    int contador = 0;
     int values[8] = {2, 5, 7, 8, 10, 13, 16, 19};
     for (int i = 0; i < 8; i++)
     {
@@ -10,9 +11,10 @@ void evenNumber(){
         if (number % 2 == 0)
         {
             cout <<number <<" es un numero par"  <<endl;
+            contador++;
         }
     }
-    
+    cout << "Hay " <<contador <<" numeros pares" <<endl;
 }
 
 
