4c10718b112e8403485a246314dbda72d2cfbc80
elianamartinez05-coder <eliana.martinez05@uptc.edu.co>
2025-10-30 16:21:17 +0000

    ejercicio 4

--- Taller#1-Array/ejercicio4.cpp
+++ Taller#1-Array/ejercicio4.cpp
@@ -2,13 +2,21 @@
 using namespace std;
 
 void evenNumber(){
-
+    int number;
     int values[8] = {2, 5, 7, 8, 10, 13, 16, 19};
+    for (int i = 0; i < 8; i++)
+    {
+        number = values[i];
+        if (number % 2 == 0)
+        {
+            cout <<number <<" es un numero par"  <<endl;
+        }
+    }
     
 }
 
 
 int main(){
-
+    evenNumber();
     return 0;
 }
